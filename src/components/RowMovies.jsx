82308c90d800c356a86834fb366ce6a541b1ea48
HocgH <57635765+HocgH@users.noreply.github.com>
2022-11-14 14:15:23 +0200

    Created a new component
    The role of this component is to simplify the code and make it more accesible!

--- src/components/RowMovies.jsx
+++ src/components/RowMovies.jsx
@@ -1,11 +1,12 @@
 import React, {useState, useEffect} from 'react';
-import {FaHeart, FaRegHeart} from 'react-icons/fa';
+
 import axios from 'axios';
+import OneMovie from './OneMovie';
 
 const RowMovies = (props) => {
   
   const [movies, setMovies] = useState([]);
-  const [saveMovie, setSaveMovie] = useState(false);
+
 
   useEffect( () => {
     axios.get(props.fetchURL).then((response) => {
@@ -23,13 +24,7 @@ const RowMovies = (props) => {
             <div id={'slider'}>
 
                 {movies.map((item, id) => (
-                    <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
-                        <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
-                        <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
-                            <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
-                            <p> {saveMovie ? <FaHeart className='absolute top-6 left-6 text-gray-400'/> : <FaRegHeart className='absolute top-6 left-6 text-gray-400'/>} </p>
-                        </div>
-                    </div>
+                    <OneMovie key = {id} item = {item}/>
                 ))}
 
             </div>
