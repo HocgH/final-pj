f890c6c3d2224ddf65fc43f28986414abfd5de17
HocgH <57635765+HocgH@users.noreply.github.com>
2022-11-14 20:29:27 +0200

    finalized the project!
    Besides the darkmode/lightmode costum hook i couldn t finalize, every feature is done!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,50 @@
 import React from 'react';
+import {Link, useNavigate} from 'react-router-dom';
+import {UserAuth} from '../context/AuthContext';
 
 const Navbar = () => {
 
+  const {user, logOut} = UserAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try{
+      await logOut();
+      navigate('/');
+    } catch(error){
+    console.log(error);
+    }
+  };
   // const [darkMode, setDarkMode] = useDarkMode();
 
   return (
     <div className='flex items-center justify-between p-6 absolute w-full z-[100]'>
-        <h1 className='text-red-700 text-4xl font-bold cursor-pointer'>NETFLIX CLONE</h1>
+
+      <Link to='/' >
         
+        <h1 className='text-red-700 text-4xl font-bold cursor-pointer'>NETFLIX CLONE</h1>
+      
+      </Link>
+
+      {user?.email ? (
         <div>
-            <button className='border text-white border-gray-300  px-5 py-2 mr-3 bg-black/50'>Sign In</button>
-            <button className='bg-red-700 px-5 py-2 cursor-pointer text-white'>Sign Up</button>
-        </div>
+          <Link to='/account'>
+            <button className='border text-white border-gray-300  px-5 py-2 mr-3 bg-black/50'>Account</button>
+          </Link>
+
+            <button onClick={handleLogout} className='bg-red-700 px-5 py-2 cursor-pointer text-white'>Logout</button>
+
+        </div> ) : (
+                <div>
+                <Link to='/login'>
+                  <button className='border text-white border-gray-300  px-5 py-2 mr-3 bg-black/50'>Sign In</button>
+                </Link>
+      
+                <Link to='/signup' >
+                  <button className='bg-red-700 px-5 py-2 cursor-pointer text-white'>Sign Up</button>
+                </Link>
+              </div>)}        
+
     </div>
   );
 }
--- src/components/OneMovie.jsx
+++ src/components/OneMovie.jsx
@@ -1,16 +1,39 @@
 import React, {useState} from 'react';
 import {FaHeart, FaRegHeart} from 'react-icons/fa';
+import {UserAuth} from '../context/AuthContext';
+import {db} from '../firebase';
+import {arrayUnion, doc, updateDoc} from 'firebase/firestore';
 
 const OneMovie = ({item}) => {
 
-    const [saveMovie, setSaveMovie] = useState(false);
+    const [heartMovie, setHearthMovie] = useState(false);
+    const [saved, setSaved] = useState(false);
+    const {user} = UserAuth();
+
+    const movieID = doc(db, 'users', `${user?.email}`);
+
+    const heartSave = async () => {
+      if(user?.email) {
+        setHearthMovie(!heartMovie);
+        setSaved(true);
+        await updateDoc(movieID, {
+          savedShows: arrayUnion({
+            id:item.id,
+            title: item.title,
+            img: item.backdrop_path
+          })
+        })
+      } else {
+        alert('Please log in to save a movie');
+      }
+    }
 
   return (
     <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
         <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title} />
             <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
                 <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{item?.title}</p>
-                <p> {saveMovie ? <FaHeart className='absolute top-6 left-6 text-gray-400'/> : <FaRegHeart className='absolute top-6 left-6 text-gray-400'/>} </p>
+                <p onClick={heartSave}> {heartMovie ? <FaHeart className='absolute top-6 left-6 text-gray-400'/> : <FaRegHeart className='absolute top-6 left-6 text-gray-400'/>} </p>
             </div>
     </div>
   )
