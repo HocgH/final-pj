877c8d985634bb270f58ea0ea3c118729e13f397
HocgH <57635765+HocgH@users.noreply.github.com>
2022-11-14 15:24:34 +0200

    styled the rows and added icons for future movement on click

--- src/components/RowMovies.jsx
+++ src/components/RowMovies.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-
+import {MdChevronLeft, MdChevronRight} from 'react-icons/md';
 import axios from 'axios';
 import OneMovie from './OneMovie';
 
@@ -19,15 +19,19 @@ const RowMovies = (props) => {
     return (
     <div>
         <h2 className='text-white font-bold md:text-xl p-4'>{props.title}</h2>
-        <div className='relative flex items-center'>
+        <div className='relative flex items-center group'>
 
-            <div id={'slider'}>
+            <MdChevronLeft className='bg-white rounded-full opacity-40 hover:opacity-90 cursor-pointer z-10 hidden group-hover:block ml-3' size={30}/>
+
+            <div id={'slider'} className=" relative w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide " >
 
                 {movies.map((item, id) => (
                     <OneMovie key = {id} item = {item}/>
                 ))}
 
             </div>
+
+            <MdChevronRight className='bg-white rounded-full opacity-40 hover:opacity-90 cursor-pointer z-10 hidden group-hover:block mr-3' size={30}/>
 
         </div>
     </div>
